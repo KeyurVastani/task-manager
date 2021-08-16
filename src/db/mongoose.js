dc12c6971e27ee1ee6363e0ba42b640e2eb78206
KeyurVastani <vastanikeyur1711@gmail.com>
2021-08-16 18:26:39 +0530

    make a api demo

---
+++ src/db/mongoose.js
@@ -0,0 +1,119 @@
+//this file is export in index.js
+
+const mongoose = require('mongoose');
+// const validator = require('validator')
+
+
+//mongoose connection
+mongoose.connect('mongodb+srv://Keyur_Vastani:Keyur_Vastani@cluster0.rh3wt.mongodb.net/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, 
+// (error) => {
+//     if (error) {
+//         return console.log("unable to connect");
+//     }
+//     console.log("connection correctly");
+// }
+)
+
+
+//mongoose model define  //model in under avelo User is a collection name
+// const User = mongoose.model("User", {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//     },
+
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error("Email is  invalid")
+//             }
+//         }
+//     },
+
+//     password: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 7,
+//         validate(value) {
+//             if (value.toLowerCase().includes('password')) {
+//                 throw new Error('password cannot contain "password"')
+//             }
+//         }
+//     },
+
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {                       //custome validator
+//             if (value < 0) {
+//                 throw new Error("you are under ages")
+//             }
+
+//         }
+//     }
+// })
+
+// //create instances of the model that actually add thedata in the database
+// const me = new User({
+//     name: "sohanna",
+//     age: 23,
+//     email: 'sohan123456@gmail.com',
+//     password: "1234ghjd"
+
+
+// })
+
+
+// //save to the database 
+
+// me.save().then(() => {
+//     console.log(me);
+
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+
+
+//--------------------------create a Task model----------------------------------------
+
+// const Task = mongoose.model("Task", {
+//     description: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         required: true,
+
+//     },
+  
+// })
+
+
+// const task1 = new Task({
+//     description: "Lunch ",
+//     completed: true
+// })
+
+
+
+
+// task1.save().then(() => {
+//     console.log(task1);
+
+// }).catch((e) => {
+//     console.log(e);
+// })
---
+++ src/index.js
@@ -0,0 +1,100 @@
+//this is a starting point of own application
+
+const express = require('express')
+require('./db/mongoose')                   //this is a connection of database
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//get the data from the client 
+app.use(express.json())
+
+
+
+// for user post the data
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+    // console.log(req.body  );
+    // res.send("testing perfectly work")
+
+    user.save().then(() => {
+        res.send(user)
+
+    }).catch((e) => {
+        res.status(400).send(e)
+        // res.send(e)
+    })
+
+
+
+})
+
+//for tasks post the data
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    // res.send("testing perfectly work")
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+
+})
+
+
+// get the user data
+
+app.get('/users', (req, res) => {
+
+    User.find({ name: 'keyur', age: '23', password: 'keyur123' }).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+
+//get data through id properties
+
+app.get('/users/:id', (req, res) => {
+    // console.log(req.params);
+
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send("user not found")
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(400).send("this id data is not available  ", _id)
+    })
+
+})
+
+
+
+
+//when you enter url is wrong
+app.post('*', (req, res) => {
+    res.status(400).send({
+        title: "This is a 404 Error",
+        errorMessage: " please try again"
+    })
+
+})
+
+app.get('*', (req, res) => {
+    res.status(400).send({
+        title: "This is a 404 Error",
+        errorMessage: " please try again"
+    })
+
+})
+
+app.listen(port, () => {
+    console.log("Server is run on port : " + port);
+
+})
